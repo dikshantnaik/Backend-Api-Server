033bbdcf4275ba61c2116eab4825ce35076ea7c2
Dikshant <dikshantlnaikxyz@gmail.com>
2021-09-17 17:49:50 +0530

    awdad

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn -w 4 -k uvicorn.workers.uvicornWorker myapi:app
-
+web: uvicorn myapi:app --port $PORT --reload
--- myapi.py
+++ myapi.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
-
+print("Starting ")
 app = FastAPI()
 
 @app.get("/")
