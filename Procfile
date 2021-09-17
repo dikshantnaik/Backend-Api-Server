f6f59641057e773ff3f10a3213d76707677c6a50
Dikshant <dikshantlnaikxyz@gmail.com>
2021-09-17 11:43:31 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn myapi.app:app --host=0.0.0.0 --port=${PORT:-5000}
---
+++ myapi.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return "heya There Bitches"
+
+@app.get("/item/{item_id}")
+def read_item(item_id: int ,q :Optional[str] = None):
+    return {"item_it" : item_id,"q":q}
