9f32aa8bf566a06c4edcc5f4c2ab213b2c49d289
Dikshant <dikshantlnaikxyz@gmail.com>
2021-09-17 12:35:25 +0530

    chg

--- myapi.py
+++ myapi.py
@@ -1,16 +1,12 @@
-# Python code to demonstrate naive method
-# to compute factorial
-n = 5
-fact = 1
+from typing import Optional
+from fastapi import FastAPI
 
-for i in range(1,n+1):
-	fact = fact * i
-	
-print ("The factorial of 23 is : ",end="")
-print (fact)
+app = FastAPI()
 
-Suppose We have to find factorial of 5 this will be inputed by user ..
-So we will declare n = 5;
-Now we will use fact variable to store the Answer which is factorial of n;
-we will also Inzialize i = 1 as an loop counter
-now we will loop step 5.1 and 5.2  till i is equal to n;
+@app.get("/")
+def index():
+    return "heya There Bitches"
+
+@app.get("/item/{item_id}")
+def read_item(item_id: int ,q :Optional[str] = None):
+    return {"item_it" : item_id,"q":q}
