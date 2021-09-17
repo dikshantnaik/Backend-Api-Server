4c171b3a2ccb6a9b182980802a1c63263bcc05cd
Dikshant <dikshantlnaikxyz@gmail.com>
2021-09-17 18:16:21 +0530

    awdad

--- myapi.py
+++ myapi.py
@@ -1,12 +1,8 @@
-from typing import Optional
 from fastapi import FastAPI
-print("Starting on")
+
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return "heya There Bitches"
 
-# @app.get("/item/{item_id}")
-# def read_item(item_id: int ,q :Optional[str] = None):
-#     return {"item_it" : item_id,"q":q}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
